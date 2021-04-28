9904def1243dd74f79b51632c3698d03616d8d82
Gnojevik <gnojevik@gmail.com>
2021-04-28 08:27:20 +0300

    add modal cart + mod functionality

--- js/functionality.js
+++ js/functionality.js
@@ -1,3 +1,5 @@
+document.getElementById('scripHeader').onload = function () { functionality() }; // чекаєм поки прогрузиться хедер
+
 function functionality() {
 
    let cart = [];
@@ -5,7 +7,6 @@ function functionality() {
    let buttonClearcart = document.getElementById('clearCard');
    buttonClearcart.addEventListener('click', clearCart);
 
-   console.log(buttonsAddToCart);
    for (let index = 0; index < buttonsAddToCart.length; index++) {
       buttonsAddToCart[index].addEventListener('click', addToCart);
    }
@@ -31,7 +32,7 @@ function functionality() {
          } else {
             findProduct.amount++;
          }
-         console.log(cart);
+
       }
    }
    // ================= функція очистки корзини =================
@@ -39,8 +40,84 @@ function functionality() {
 
       count.innerText = 0;
       cart = [];
-      console.log('cart cleared');
    }
+
+   // ============ Show cart =====================
+   let modal = document.getElementById('modal-cart');
+   let showCart = document.getElementById('show-card');
+   let closeSpan = document.getElementsByClassName('close')[0];
+
+   showCart.addEventListener('click', modalCart);
+
+   function modalCart(event) {
+      let str = '';
+      if (cart.length < 1) {
+         str += '<p class = "row"> 0 products in cart :( </p>';
+      } else {
+         for (let i = 0; i < cart.length; i++) {
+            str += `<article id="${cart[i].product.id}" class="row tablet">
+            <p class="col">${cart[i].product.name}</p>
+            <p class="col">&#36;<span>${cart[i].product.price.toFixed(2)}</span></p>
+            <input class="product-amount col" type="number" min="1" max="100" value="${cart[i].amount}">
+            <p class="col">&#36;<span> ${(cart[i].amount * cart[i].product.price).toFixed(2)} </span></p>
+         </article>`
+
+         }
+      }
+
+      document.getElementById('cart-body').innerHTML = str;
+      modal.style.display = "block";
+
+      let productAmountInputs = document.getElementsByClassName('product-amount');
+      for (let i = 0; i < productAmountInputs.length; i++) {
+         productAmountInputs[i].addEventListener('input', priceByProduct);
+
+      }
+
+      totalPrice();
+   }
+
+   // сумма одного продукту
+   function priceByProduct(event) {
+      let newAmount = event.target.value;
+      let id = event.target.parentNode.id;
+
+      let product = cart.find(item => Number(item.product.id) === Number(id));
+
+      if (product) {
+         product.amount = newAmount;
+      }
+      modalCart();
+   }
+
+   //  totalPrice 
+
+   function totalPrice() {
+      let sum = 0;
+      let newCountCart = 0;
+      for (let i = 0; i < cart.length; i++) {
+         sum += Number(cart[i].product.price) * Number(cart[i].amount);
+         newCountCart += Number(cart[i].amount);
+
+      }
+      document.getElementById('totalPrice').innerText = sum.toFixed(2);
+      count.innerText = newCountCart; // переписуємо нову кількість продуктів у кошику що на головній
+   }
+
+   //  закриваєм модалку по кнопці спан 
+
+   closeSpan.onclick = function () {
+      modal.style.display = "none";
+   }
+   //  закриваєм модалку по кліку за межами модалки
+
+   window.onclick = function (event) {
+      if (event.target == modal) {
+         modal.style.display = "none";
+      }
+   }
+
+   // ==================================================
 }
 
 // ============ button top 800px ========================
--- js/headerAndFooter.js
+++ js/headerAndFooter.js
@@ -1,7 +1,8 @@
 $("#header").load("./ajax/header.html", function () {
-   console.log('header load successfully');
+
 });
 
 $("#footer").load("./ajax/footer.html", function () {
-   console.log('footer load successfully');
+
 });
+
